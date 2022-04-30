1f3f0318638f009e3eaf6c09f9dbe738a1cdd7e4
ryrego <ryrego3@aol.com>
2022-04-30 15:38:08 -0500

    hello

--- go.bat
+++ go.bat
@@ -1,4 +1,4 @@
 set PATH="C:\Program Files\Java\jdk1.8.0_74\bin"
-REM  hi
+REM  hello
 REM  arg1 = start word,     arg2 = X, Y, G    where X is 'does not  contain'   Y is 'contains but not in this place'   G is 'correct'
 java mymain %1 %2
